4632c717467fa9280b77f64d67deeeecfd21d988
Stuart Kershaw <stuartdkershaw@gmail.com>
2017-08-31 15:46:26 -0700

    lab 14 deep dive on 2nd 1/2 of TODOs

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -5,7 +5,8 @@ var app = app || {};
   const repos = {};
   repos.all = [];
 
-  // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // DONE: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  //  Makes a get request to the github API, which triggers proxyGitHub which handles token athentication and routing for /github/*. The, data returned from github is pushed to array, repos.all.  repoView.index is callback. Called from aboutController.js.
   repos.requestRepos = function(callback) {
     $.get('/github/user/repos')
     .then(data => repos.all = data, err => console.error(err))
--- starter-code/public/scripts/views/adminView.js
+++ starter-code/public/scripts/views/adminView.js
@@ -5,7 +5,8 @@ var app = app || {};
   const adminView = {
     initAdminPage : () => {
       let template = Handlebars.compile($('#author-template').text());
-        // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+        // DONE: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+      // app.Article.numWordsByAuthor() returns authors and words for each. the .forEach acts on those results setting a 'stat' iterator, appending that into $('author-stats') as an argument to template(), which populates the Handlebars template.
       app.Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
       $('#blog-stats .articles').text(app.Article.all.length);
       $('#blog-stats .words').text(app.Article.numWordsAll());
